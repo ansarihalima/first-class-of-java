363e6db1c20c5b25ec68e45cee94b5e242b8127f
ansarihalima <halimasadiahalimasadia00@gmail.com>
2024-04-23 01:26:30 -0700

    practise

---
+++ index.js
@@ -0,0 +1,46 @@
+alert("HELLO ! CODER")
+// Alert("hello coder") capital A not working bcz javascript is case sensitive!
+
+
+
+//  Single line comment
+
+/*
+  multi line comment
+  */
+ 
+
+  //Variables
+
+  var username = 'Ansari Halima';
+  console.log("HELLO" , username)
+  console.log(username)
+  console.log(username)
+  console.log(username)
+  console.log(username)
+  console.log(username)
+  console.log(username)
+  console.log(username)
+
+
+
+  //for over writing
+  document.write("its not good foe over write")
+
+
+  // = ---> assignment operator
+
+// var email; // declaration
+  email = "abcd@gmail.com"; //initialization / assignment
+  console.log(email);
+
+
+   var description = "my description";
+ description = "my updated description"; //reassign
+ description = "my updated description 2"
+console.log(description);
+
+// meaning ful variable name 
+
+// var x = "RAZA"; not meaning full
+// var username = "RAZA";
